3ecacaa3f8df050c11dd4a11cfc0c4b33d4e660b
lukas <lukasvinclav@gmail.com>
2023-01-10 21:04:00 +0100

    feat: ruff integration

--- src/unfold/forms.py
+++ src/unfold/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.admin.forms import AdminAuthenticationForm
+from django.contrib.admin.forms import (
+    AdminAuthenticationForm,
+)
 from django.contrib.admin.forms import (
     AdminPasswordChangeForm as BaseAdminOwnPasswordChangeForm,
 )
