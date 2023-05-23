f5a962a5aa2b835e038b6339820bb7de88621e5d
Pavel Pančocha <pp@zemtu.com>
2023-05-23 21:04:03 +0200

    feat: add testing suite
    - Add GitHub actions

--- src/unfold/runtests.py
+++ src/unfold/runtests.py
@@ -2,16 +2,34 @@ import os
 import sys
 
 import django
+from django.test.runner import DiscoverRunner
 
-if __name__ == "__main__":
+
+def print_versions():
+    version_info = f"""
+    ** Running tests...
+    ** Using Python=={sys.version}
+         from {sys.executable}
+    ** Using Django=={django.get_version()}
+    """
+    print(version_info)
+
+
+def setup_django():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     django.setup()
-    from django.test.runner import DiscoverRunner
 
+
+def run_tests():
     test_runner = DiscoverRunner(
         verbosity=2, interactive=False, shuffle=os.getenv("RANDOM_SEED")
     )
-    print(f"Testing against Django=={django.get_version()}, Python=={sys.version}")
     failures = test_runner.run_tests(["unfold.tests"])
     if failures:
         sys.exit(failures)
+
+
+if __name__ == "__main__":
+    print_versions()
+    setup_django()
+    run_tests()
