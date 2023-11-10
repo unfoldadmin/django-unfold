d35ce019469753a7614a143b615299cf5245753d
Lukas Vinclav <lukasvinclav@gmail.com>
2023-11-10 19:54:25 +0100

    feat: environment label (#189)

---
+++ tests/test_environment.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.test.utils import override_settings
+from unfold.settings import CONFIG_DEFAULTS, get_config
+from unfold.sites import UnfoldAdminSite
+
+
+def environment_callback(request):
+    return ["Testing Environment", "warning"]
+
+
+class EnvironmentTestCase(TestCase):
+    @override_settings(UNFOLD={**CONFIG_DEFAULTS})
+    def test_empty_environment_callback(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        self.assertTrue("environment" not in context)
+        get_config.cache_clear()
+
+    @override_settings(
+        UNFOLD={
+            **CONFIG_DEFAULTS,
+            **{
+                "ENVIRONMENT": "tests.test_environment.non_existing_environment_callback",
+            },
+        }
+    )
+    def test_incorrect_environment_callback(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        print(context)
+        self.assertTrue("environment" not in context)
+        get_config.cache_clear()
+
+    @override_settings(
+        UNFOLD={
+            **CONFIG_DEFAULTS,
+            **{
+                "ENVIRONMENT": "tests.test_environment.environment_callback",
+            },
+        }
+    )
+    def test_correct_environment_callback(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        print(context)
+        self.assertTrue("environment" in context)
+        self.assertEqual(context["environment"], ["Testing Environment", "warning"])
+        get_config.cache_clear()
