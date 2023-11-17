edb5932f2d8914f79790122099799f600a53be6b
Lukas Vinclav <lukasvinclav@gmail.com>
2023-11-17 12:31:31 +0100

    feat: show/hide history & view on site (#201)

---
+++ tests/test_show.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.test.utils import override_settings
+from unfold.settings import CONFIG_DEFAULTS, get_config
+from unfold.sites import UnfoldAdminSite
+
+
+class ShowTestCase(TestCase):
+    @override_settings(UNFOLD={**CONFIG_DEFAULTS})
+    def test_show_history_default(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        self.assertTrue(context.get("show_history"))
+        get_config.cache_clear()
+
+    @override_settings(UNFOLD={**CONFIG_DEFAULTS, **{"SHOW_HISTORY": False}})
+    def test_show_history_hide(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        self.assertFalse(context.get("show_history"))
+        get_config.cache_clear()
+
+    @override_settings(UNFOLD={**CONFIG_DEFAULTS})
+    def test_show_view_on_site_default(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        self.assertTrue(context.get("show_view_on_site"))
+        get_config.cache_clear()
+
+    @override_settings(UNFOLD={**CONFIG_DEFAULTS, **{"SHOW_VIEW_ON_SITE": False}})
+    def test_show_view_on_site_hide(self):
+        admin_site = UnfoldAdminSite()
+        request = RequestFactory().get("/rand")
+        request.user = AnonymousUser()
+        context = admin_site.each_context(request)
+        self.assertFalse(context.get("show_view_on_site"))
+        get_config.cache_clear()
