44a1419209f3695a814e04b3c264d00aff848f2d
lukas <lukasvinclav@gmail.com>
2023-12-03 09:55:08 +0100

    feat: hex to rgb

--- src/unfold/utils.py
+++ src/unfold/utils.py
@@ -111,3 +111,13 @@ def display_for_field(value: Any, field: Any, empty_value_display: str) -> str:
             return display_for_value(value, empty_value_display)
     else:
         return display_for_value(value, empty_value_display)
+
+
+def hex_to_rgb(hex_color: str) -> list[int]:
+    hex_color = hex_color.lstrip("#")
+
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+
+    return (r, g, b)
