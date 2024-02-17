2ad558e3ccd1581c192fcbe4eaa898aec225a7cd
Lukas Vinclav <lukasvinclav@gmail.com>
2024-02-17 08:16:20 +0100

    fix: range filters wrong arguments (#293)

--- src/unfold/contrib/filters/admin.py
+++ src/unfold/contrib/filters/admin.py
@@ -57,7 +57,10 @@ class SingleNumericFilter(admin.FieldListFilter):
 
         if self.parameter_name in params:
             value = params.pop(self.parameter_name)
-            self.used_parameters[self.parameter_name] = value
+            value = value[0] if isinstance(value, list) else value
+
+            if value not in EMPTY_VALUES:
+                self.used_parameters[self.parameter_name] = value
 
     def queryset(
         self, request: HttpRequest, queryset: QuerySet[Any]
@@ -94,11 +97,16 @@ class RangeNumericMixin:
     def init_used_parameters(self, params: Dict[str, Any]) -> None:
         if self.parameter_name + "_from" in params:
             value = params.pop(self.parameter_name + "_from")
-            self.used_parameters[self.parameter_name + "_from"] = value
+
+            self.used_parameters[self.parameter_name + "_from"] = (
+                value[0] if isinstance(value, list) else value
+            )
 
         if self.parameter_name + "_to" in params:
             value = params.pop(self.parameter_name + "_to")
-            self.used_parameters[self.parameter_name + "_to"] = value
+            self.used_parameters[self.parameter_name + "_to"] = (
+                value[0] if isinstance(value, list) else value
+            )
 
     def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet:
         filters = {}
@@ -302,11 +310,17 @@ class RangeDateFilter(admin.FieldListFilter):
 
         if self.parameter_name + "_from" in params:
             value = params.pop(self.field_path + "_from")
-            self.used_parameters[self.field_path + "_from"] = value
+            value = value[0] if isinstance(value, list) else value
+
+            if value not in EMPTY_VALUES:
+                self.used_parameters[self.field_path + "_from"] = value
 
         if self.parameter_name + "_to" in params:
             value = params.pop(self.field_path + "_to")
-            self.used_parameters[self.field_path + "_to"] = value
+            value = value[0] if isinstance(value, list) else value
+
+            if value not in EMPTY_VALUES:
+                self.used_parameters[self.field_path + "_to"] = value
 
     def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet:
         filters = {}
