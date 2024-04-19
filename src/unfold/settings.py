bcda6ee1d47a24bdfe5ce95a417574bec5e058e8
ElebrUS <megaelebrus@ya.ru>
2024-04-19 13:57:01 +0300

    Clean-UP Settings

--- src/unfold/settings.py
+++ src/unfold/settings.py
@@ -9,7 +9,6 @@ CONFIG_DEFAULTS = {
     "SITE_LOGO": None,
     "SHOW_HISTORY": True,
     "SHOW_VIEW_ON_SITE": True,
-    "FIELDSET_CLASSES": ["tab"],
     "COLORS": {
         "primary": {
             "50": "250 245 255",
