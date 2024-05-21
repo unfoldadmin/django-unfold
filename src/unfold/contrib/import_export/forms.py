ec463e30dcba8aa58ff795300a48329841b25ca8
Zair Mahmood <7028968+ZairMahmood@users.noreply.github.com>
2024-05-21 15:44:24 -0700

    fix: django-import-export unable to import

--- src/unfold/contrib/import_export/forms.py
+++ src/unfold/contrib/import_export/forms.py
@@ -1,15 +1,29 @@
-from import_export.forms import ImportExportFormBase as BaseImportExportFormBase
-from unfold.widgets import SELECT_CLASSES, UnfoldAdminFileFieldWidget
+from import_export.forms import ExportForm as ExportFormBase
+from import_export.forms import ImportForm as ImportFormBase
+from unfold.widgets import UnfoldAdminFileFieldWidget, UnfoldAdminSelectWidget
 
 
-class ImportForm(BaseImportExportFormBase):
+class ImportForm(ImportFormBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["resource"].widget = UnfoldAdminFileFieldWidget()
-        self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+        resource_widget = self.fields["resource"].widget
+        if len(resource_widget.choices) > 0:
+            self.fields["resource"].widget = UnfoldAdminSelectWidget(
+                attrs=resource_widget.attrs, choices=resource_widget.choices
+            )
+        else:
+            self.fields.pop("resource")
+        self.fields["import_file"].widget = UnfoldAdminFileFieldWidget()
+        format_widget = self.fields["format"].widget
+        self.fields["format"].widget = UnfoldAdminSelectWidget(
+            attrs=format_widget.attrs, choices=format_widget.choices
+        )
 
 
-class ExportForm(BaseImportExportFormBase):
+class ExportForm(ExportFormBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+        format_widget = self.fields["format"].widget
+        self.fields["format"].widget = UnfoldAdminSelectWidget(
+            attrs=format_widget.attrs, choices=format_widget.choices
+        )
