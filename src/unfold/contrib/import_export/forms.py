76022c310ef30aa05c891db67d772d76ca6d8b48
Lukas Vinclav <lukasvinclav@gmail.com>
2024-05-23 11:50:16 +0200

    fix: import export 4 support (#404)

--- src/unfold/contrib/import_export/forms.py
+++ src/unfold/contrib/import_export/forms.py
@@ -1,15 +1,21 @@
-from import_export.forms import ImportExportFormBase as BaseImportExportFormBase
-from unfold.widgets import SELECT_CLASSES, UnfoldAdminFileFieldWidget
+from import_export.forms import ExportForm as BaseExportForm
+from import_export.forms import ImportForm as BaseImportForm
+from unfold.widgets import (
+    SELECT_CLASSES,
+    UnfoldAdminFileFieldWidget,
+)
 
 
-class ImportForm(BaseImportExportFormBase):
+class ImportForm(BaseImportForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["resource"].widget = UnfoldAdminFileFieldWidget()
+
+        self.fields["resource"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+        self.fields["import_file"].widget = UnfoldAdminFileFieldWidget()
         self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
 
 
-class ExportForm(BaseImportExportFormBase):
+class ExportForm(BaseExportForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
