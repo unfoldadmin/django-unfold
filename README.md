e6b48af0f9d8ad01c3c7d04d1a1202f55891a5ec
Lukas Vinclav <lukasvinclav@gmail.com>
2024-06-03 08:16:00 +0200

    fix: import export selectable fields (#431)

--- README.md
+++ README.md
@@ -839,11 +839,12 @@ Adding support for django-guardian is quote straightforward in Unfold, just add 
 
 from unfold.admin import ModelAdmin
 from import_export.admin import ImportExportModelAdmin
-from unfold.contrib.import_export.forms import ExportForm, ImportForm
+from unfold.contrib.import_export.forms import ExportForm, ImportForm, SelectableFieldsExportForm
 
 class ExampleAdmin(ModelAdmin, ImportExportModelAdmin):
     import_form_class = ImportForm
     export_form_class = ExportForm
+    # export_form_class = SelectableFieldsExportForm
 ```
 
 When implementing `import_export.admin.ExportActionModelAdmin` class in admin panel, import_export plugin adds its own implementation of action form which is not incorporating Unfold CSS classes. For this reason, `unfold.contrib.import_export.admin` contains class with the same name `ExportActionModelAdmin` which inherits behavior of parent form and adds appropriate CSS classes.
--- src/unfold/contrib/import_export/forms.py
+++ src/unfold/contrib/import_export/forms.py
@@ -1,6 +1,14 @@
+from django.forms.fields import BooleanField
 from import_export.forms import ExportForm as BaseExportForm
 from import_export.forms import ImportForm as BaseImportForm
-from unfold.widgets import SELECT_CLASSES, UnfoldAdminFileFieldWidget
+from import_export.forms import (
+    SelectableFieldsExportForm as BaseSelectableFieldsExportForm,
+)
+from unfold.widgets import (
+    SELECT_CLASSES,
+    UnfoldAdminFileFieldWidget,
+    UnfoldBooleanWidget,
+)
 
 
 class ImportForm(BaseImportForm):
@@ -17,3 +25,14 @@ class ExportForm(BaseExportForm):
         super().__init__(*args, **kwargs)
         self.fields["resource"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
         self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+
+
+class SelectableFieldsExportForm(BaseSelectableFieldsExportForm):
+    def __init__(self, formats, resources, **kwargs):
+        super().__init__(formats, resources, **kwargs)
+        self.fields["resource"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+        self.fields["format"].widget.attrs["class"] = " ".join(SELECT_CLASSES)
+
+        for _key, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget = UnfoldBooleanWidget()
