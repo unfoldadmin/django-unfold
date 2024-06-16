93e2cf8c6e5ff1c4fdc92e11b4d17c389d2140dc
Elyas Ebrahimpour <eliasebrahimpour@gmail.com>
2024-06-16 20:35:20 +0330

    add variable USE_MULTILANGUAGE to config

--- src/unfold/settings.py
+++ src/unfold/settings.py
@@ -9,6 +9,7 @@ CONFIG_DEFAULTS = {
     "SITE_LOGO": None,
     "SHOW_HISTORY": True,
     "SHOW_VIEW_ON_SITE": True,
+    "USE_MULTILANGUAGE": False,
     "COLORS": {
         "primary": {
             "50": "250 245 255",
