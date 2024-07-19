59222e2c5975088b2a531945a8b59d9211471964
Ali Yaman <aliymn.db@gmail.com>
2024-07-19 22:00:06 +0200

    add 127.0.0.1 to ALLOWED_HOSTS

--- tests/server/example/settings.py
+++ tests/server/example/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = environ.get("SECRET_KEY", get_random_secret_key())
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 AUTH_USER_MODEL = "example.User"
 
