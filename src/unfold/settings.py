ad187162b4cafe8b57c8aa41357dc96a91363ec4
Lukas Vinclav <lukasvinclav@gmail.com>
2024-09-05 13:35:21 +0200

    feat: configurable font colors (#718)

--- src/unfold/settings.py
+++ src/unfold/settings.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from django.conf import settings
 
 CONFIG_DEFAULTS = {
@@ -11,6 +13,14 @@ CONFIG_DEFAULTS = {
     "SHOW_HISTORY": True,
     "SHOW_VIEW_ON_SITE": True,
     "COLORS": {
+        "font": {
+            "subtle-light": "107 114 128",  # text-gray-500
+            "subtle-dark": "156 163 175",  # text-gray-400
+            "default-light": "75 85 99",  # text-gray-600
+            "default-dark": "209 213 219",  # text-gray-300
+            "important-light": "17 24 39",  # text-gray-900
+            "important-dark": "243 244 246",  # text-gray-100
+        },
         "primary": {
             "50": "250 245 255",
             "100": "243 232 255",
@@ -47,4 +57,19 @@ def get_config(settings_name=None):
     if settings_name is None:
         settings_name = "UNFOLD"
 
-    return {**CONFIG_DEFAULTS, **getattr(settings, settings_name, {})}
+    def merge_dicts(dict1: Dict[str, Any], dict2: Dict[str, Any]) -> Dict[str, Any]:
+        result = dict1.copy()
+
+        for key, value in dict2.items():
+            if (
+                key in result
+                and isinstance(result[key], dict)
+                and isinstance(value, dict)
+            ):
+                result[key] = merge_dicts(result[key], value)
+            else:
+                result[key] = value
+
+        return result
+
+    return merge_dicts(CONFIG_DEFAULTS, getattr(settings, settings_name, {}))
