37f7b31454385e90ebf404e63e1dfe7671c868e0
lukas <lukasvinclav@gmail.com>
2025-02-05 10:36:35 +0100

    fix: active tab item

--- src/unfold/sites.py
+++ src/unfold/sites.py
@@ -1,3 +1,4 @@
+import copy
 from http import HTTPStatus
 from typing import Any, Callable, Optional, Union
 from urllib.parse import parse_qs, urlparse
@@ -278,7 +279,7 @@ class UnfoldAdminSite(AdminSite):
         return results
 
     def get_tabs_list(self, request: HttpRequest) -> list[dict[str, Any]]:
-        tabs = self._get_config("TABS", request)
+        tabs = copy.deepcopy(self._get_config("TABS", request))
 
         if not tabs:
             return []
