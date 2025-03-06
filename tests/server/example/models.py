d5f18ba9b9547dc65cab380047d2dcef16e614a7
Zerotask <patrick.hintermayer@openesg.de>
2025-03-06 12:54:08 +0100

    test(filters): add test_filters_slider_numeric_filter

--- tests/server/example/models.py
+++ tests/server/example/models.py
@@ -6,3 +6,4 @@ class User(AbstractUser):
     content_type = models.ForeignKey(
         "contenttypes.ContentType", on_delete=models.CASCADE, null=True, blank=True
     )
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
