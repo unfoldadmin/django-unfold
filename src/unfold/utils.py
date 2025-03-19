b0ca519f8eca2bfcb42aa3599d641fd605332827
Zerotask <patrick.hintermayer@openesg.de>
2025-03-19 09:43:18 +0100

    feat(json): allow rendering of custom JSONFields which uses django form JSONField as form_class

--- src/unfold/utils.py
+++ src/unfold/utils.py
@@ -130,12 +130,12 @@ def display_for_field(value: Any, field: Any, empty_value_display: str) -> str:
         return formats.number_format(value, field.decimal_places)
     elif isinstance(field, (models.IntegerField, models.FloatField)):
         return formats.number_format(value)
+    elif isinstance(field, models.FileField) and value:
+        return format_html('<a href="{}">{}</a>', value.url, value)
     elif (
-        isinstance(field, models.FileField)
+        isinstance(field, models.JSONField)
         or isinstance(field.formfield(), fields.JSONField)
     ) and value:
-        return format_html('<a href="{}">{}</a>', value.url, value)
-    elif isinstance(field, models.JSONField) and value:
         try:
             return json.dumps(value, ensure_ascii=False, cls=field.encoder)
         except TypeError:
