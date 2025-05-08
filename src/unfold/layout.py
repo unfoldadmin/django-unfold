91187b19feedb13659c246b0f2f2d6ccfa5b98e6
Lukas Vinclav <lukasvinclav@gmail.com>
2025-05-08 08:51:43 +0200

    feat: crispy forms horizontal rule (#1271)

--- src/unfold/layout.py
+++ src/unfold/layout.py
@@ -1,4 +1,6 @@
-from crispy_forms.layout import BaseInput
+from crispy_forms.layout import BaseInput, LayoutObject
+from crispy_forms.utils import TEMPLATE_PACK
+from django.template.loader import render_to_string
 
 from unfold.widgets import BUTTON_CLASSES
 
@@ -21,3 +23,19 @@ class Submit(ButtonClassesMixin, BaseInput):
 
 class Button(ButtonClassesMixin, BaseInput):
     input_type = "button"
+
+
+class Hr(LayoutObject):
+    template = "unfold_crispy/layout/hr.html"
+
+    def __init__(self, title=None, *args, **kwargs):
+        self.title = title
+        super().__init__(*args, **kwargs)
+
+    def render(self, form, context, template_pack=TEMPLATE_PACK, **kwargs):
+        return render_to_string(
+            self.template,
+            {
+                "title": self.title,
+            },
+        )
