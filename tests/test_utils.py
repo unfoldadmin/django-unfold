7092dd4793cc4da304724b2ea4a0a1321919c35e
lukas <lukasvinclav@gmail.com>
2025-06-21 18:21:04 +0200

    fix: python3.9 union operator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ from django.utils.translation import override
 from djmoney.models.fields import MoneyField
 from djmoney.money import Money
 
+from unfold.templatetags.unfold import element_classes
 from unfold.utils import display_for_field
 
 
@@ -40,3 +41,25 @@ def test_display_for_field_money():
         # Test with None value
         result_none = display_for_field(None, MoneyField(), empty_value_display="-")
         assert result_none == "-"
+
+
+def test_element_classes_string():
+    context = {
+        "element_classes": {
+            "test": "test-class",
+        },
+    }
+
+    result = element_classes(context, "test")
+    assert result == "test-class"
+
+
+def test_element_classes_list():
+    context = {
+        "element_classes": {
+            "test": ["test-class", "test-class-2"],
+        },
+    }
+
+    result = element_classes(context, "test")
+    assert result == "test-class test-class-2"
