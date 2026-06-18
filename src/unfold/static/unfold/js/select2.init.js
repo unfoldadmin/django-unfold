235fc75be89d26bfc3f01cc553d6b6060b52058f
lukas <lukasvinclav@gmail.com>
2026-06-18 10:32:11 +0200

    chore: JS files formatting

--- src/unfold/static/unfold/js/select2.init.js
+++ src/unfold/static/unfold/js/select2.init.js
@@ -1,46 +1,46 @@
-"use strict";
 {
-  const $ = django.jQuery;
+	const $ = django.jQuery;
 
-  $.fn.djangoCustomSelect2 = function () {
-    $.each(this, function (i, element) {
-      if (element.id.match(/__prefix__/)) {
-        return;
-      }
+	$.fn.djangoCustomSelect2 = function () {
+		$.each(this, (_index, element) => {
+			if (element.id.match(/__prefix__/)) {
+				return;
+			}
 
-      if ($(element).hasClass("select2-hidden-accessible")) {
-        return;
-      }
+			if ($(element).hasClass("select2-hidden-accessible")) {
+				return;
+			}
 
-      $(element).select2();
-    });
+			$(element).select2();
+		});
 
-    return this;
-  };
+		return this;
+	};
 
-  $.fn.djangoFilterSelect2 = function () {
-    $.each(this, function (i, element) {
-      $(element).select2({
-        closeOnSelect: !element.multiple,
-        ajax: {
-          data: (params) => {
-            return {
-              term: params.term,
-              page: params.page,
-              app_label: element.dataset.appLabel,
-              model_name: element.dataset.modelName,
-              field_name: element.dataset.fieldName,
-            };
-          },
-        },
-      });
-    });
-    return this;
-  };
+	$.fn.djangoFilterSelect2 = function () {
+		$.each(this, (_index, element) => {
+			$(element).select2({
+				closeOnSelect: !element.multiple,
+				ajax: {
+					data: (params) => {
+						return {
+							term: params.term,
+							page: params.page,
+							app_label: element.dataset.appLabel,
+							model_name: element.dataset.modelName,
+							field_name: element.dataset.fieldName,
+						};
+					},
+				},
+			});
+		});
 
-  $(function () {
-    $(".unfold-admin-autocomplete").djangoCustomSelect2();
+		return this;
+	};
 
-    $(".unfold-filter-autocomplete").djangoFilterSelect2();
-  });
+	$(() => {
+		$(".unfold-admin-autocomplete").djangoCustomSelect2();
+
+		$(".unfold-filter-autocomplete").djangoFilterSelect2();
+	});
 }
